324fa6cdce4990868feda66bb9d7bfd158436e38
Jason Chen <447462131@qq.com>
2024-04-22 17:06:33 +0800

    feat: update api

--- src/api/request_data/block_request.jsx
+++ src/api/request_data/block_request.jsx
@@ -175,7 +175,7 @@ export const accounts = (data) => {
 };
 // 在线验证者查询
 export const onlineAddr = () => {
-    return request.get(`https://www.erbiescan.io/upload/onlineAddr.json`);
+    return request.get(`/upload/onlineAddr.json`);
 };
 //creator查询
 export const creator = (data) => {
